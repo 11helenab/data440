7931b9c742fe7f1554ff10033b491225f845caed
11helenab <124002641+11helenab@users.noreply.github.com>
2026-01-28 13:49:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from data440!")
+from src.data import make_data, save_data
+from src.viz import make_figure
 
+def main():
+    data = make_data()
+    save_data(data, 'test2')
+    make_figure(data, 'test_figure.svg')
 
 if __name__ == "__main__":
     main()
